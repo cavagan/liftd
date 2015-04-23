c58db4766eee87368e75f346fe37f21ffe052cb4
unknown <jtyler@WLT001.wlt.local>
2015-04-23 17:43:51 -0400

    trying to clean up callback error from app.js. liftd.js was place holder. it had callback from post route

--- liftd.js
+++ liftd.js
@@ -1,1 +1,19 @@
 // liftd responder.
+module.exports = function (req, res, next) {
+  var userName = req.body.user_name;
+  var userText = req.body.text.split(' ');
+ var curmudgName = userText[1];
+ var curmudgScore = userText[0];
+  
+  var botPayload = {
+    text : ' Thank you. I have given ' + curmudgName + ' an additional curmudgeon score of  ' + curmudgScore + ' umm also DB says '
+					};
+ 
+  // avoid infinite loop
+  if (userName !== 'slackbot' ) {
+	  
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+}
