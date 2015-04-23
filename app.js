8e501e4ccdd577faa9589d82c9a88eadc63026c5
unknown <jtyler@WLT001.wlt.local>
2015-04-23 13:32:34 -0400

    liftd core and app.js switched names.

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ellipsisbot = require('./liftdListen');
+var app = express();
+var port = process.env.PORT || 8181;
+
+
+
+//why no commit?
+ 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// test route
+app.get('/', function (req, res) { res.status(200).send('You damn shit!') });
+
+
+ // Route that listens for a POST
+ app.post('/liftd',liftdListen);
+ 
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('liftd listening on port ' + port);
+});
