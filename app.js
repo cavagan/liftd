fed9039551b17eb885496b530fde6883ff5b0246
unknown <jtyler@WLT001.wlt.local>
2015-04-24 10:22:34 -0400

    go back to square one. express,body parser and simple handler. Root route.

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 var express = require('express')
-, async = require('async')
- , http = require('http');
- var bodyParser = require('body-parser');
-
- 
- 
+var bodyParser = require('body-parser');
 var app = express();
 
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', process.env.PORT || 8181);
 
-app.use(express.static(__dirname + '/html'));
+//handle GET requests
+//app.use(express.static(__dirname + '/html'));
 
-//app.use(express.bodyParser());
+//router  to handle POST
+app.post('/',function(request,response,){
 
-app.post('/liftd/',function(request,response,next){
-
-   var ping=request.body.txt;
+   var ping = request.body.txt;
    console.log(ping);
 
 } );
 
-
-http.createServer(app).listen(app.get('port'), function(){
- console.log('Express server listening on port ' + app.get('port'));
+//start listening
+app.listen(port, function () {
+  console.log('liftd listening on port ' + port);
 });
