774e52208c9515a142a5cca4a94c130dd4f0825c
unknown <jtyler@WLT001.wlt.local>
2015-04-24 09:07:37 -0400

    set liftd.js to write toconsole when ping is received. Ping is a post with a txt field

--- liftd.js
+++ liftd.js
@@ -1,19 +1,22 @@
 // liftd responder.
 module.exports = function (req, res, next) {
-  var userName = req.body.user_name;
-  var userText = req.body.text.split(' ');
- var curmudgName = userText[1];
- var curmudgScore = userText[0];
+  //var userName = req.body.user_name;
+  //var userText = req.body.text.split(' ');
+ //var curmudgName = userText[1];
+ //var curmudgScore = userText[0];
+ var text = req.body.txt;
   
   var botPayload = {
-    text : ' Thank you. I have given ' + curmudgName + ' an additional curmudgeon score of  ' + curmudgScore + ' umm also DB says '
+    text : ' got a ping '
 					};
  
+ console.log(botPayload);
   // avoid infinite loop
-  if (userName !== 'slackbot' ) {
+ /* if (userName !== 'slackbot' ) {
 	  
     return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();
   }
+  /*
 }
